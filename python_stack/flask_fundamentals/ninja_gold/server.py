aef173e6d597007d9a18855dce168d8b829ad32f
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-05-13 21:46:54 -0700

    updated ninja gold to display actions as they're made using jquery

--- python_stack/flask_fundamentals/ninja_gold/server.py
+++ python_stack/flask_fundamentals/ninja_gold/server.py
@@ -14,38 +14,53 @@ def index():
 
 @app.route('/process_money', methods=['POST'])
 def process_money():
-	print "Money Processed"
 
 	session['building'] = request.form['building']
+
+	if (session.get('activity_box') == None):
+		session['activity_box'] = "Welcome New Player"
+	
+	if (session.get('act_count') == None):	
+		session['act_count'] = 0
+
+	if (session.get('act_scroll') == None):
+		session['act_scroll'] = session['activity_box']
 
 	if (session['building'] == 'farm'):
 		roll = random.randrange(10, 20)
 		session['gold_amt'] += roll
 		activity = "Earned "+str(roll)+" gold from the farm!"
-		print activity
+		session['activity_box'] = activity
+		session['act_count'] += 1
+		session['act_scroll'] += activity
 
 	if (session['building'] == 'cave'):
 		roll = random.randrange(5, 10)
 		session['gold_amt'] += roll
 		activity = "Earned "+str(roll)+" gold from the cave!"
-		print activity
+		session['activity_box'] = activity
+		session['act_count'] += 1
 
 	if (session['building'] == 'house'):
 		roll = random.randrange(2, 5)
 		session['gold_amt'] += roll
 		activity = "Earned "+str(roll)+" gold from the house!"
-		print activity
+		session['activity_box'] = activity
+		session['act_count'] += 1
 
 	if (session['building'] == 'casino'):
 		roll = random.randrange(-50, 50)
 		session['gold_amt'] += roll
 		if (roll >= 0):
 			activity = "Earned "+str(roll)+" gold from the casino! You're lucky!"
+			session['activity_box'] = activity
+			session['act_count'] += 1
 		if (roll < 0):
 			activity = "Lost "+str(roll)+" gold from the casnio! Better luck next time..."
-		print activity
+			session['activity_box'] = activity
+			session['act_count'] += 1
 
-	return redirect('/')
+	return render_template("index.html")
 
 app.run(debug=True)
 
