3ff994a01d063a6ea9e1b7c155b804b4b22e32ec
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-05-15 21:13:56 -0700

    Added error messages for empty name, comment, or comment > 120 char

--- python_stack/flask_fundamentals/dojo_survey_with_validation/dojo_survey_with_validation.py
+++ python_stack/flask_fundamentals/dojo_survey_with_validation/dojo_survey_with_validation.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret' # you need to set a secret key for security purposes
 
 @app.route('/')
 def landing_page():
@@ -14,16 +17,23 @@ def users():
 	language = request.form['language']
 
 	if (len(name) == 0):
-		# display an error
+		# display an error for empty name field
+		session['error'] = "<p style='color:red;'>Error, name field empty.  Please try again.</p>"
 		return redirect('/')
 
 	if (len(comment) == 0):
-		# display an error
+		# display an error for empty comment field
+		session['error'] = "<p style='color:red;'>Error, comment field empty.  Please try again.</p>"
 		return redirect('/')
 
 	if (len(comment) > 120):
-		#display an error
+		#display an error for comment field > 120 characters
+		session['error'] = "<p style='color:red;'>Error, comment field must be < 120 characters.  Please try again.</p>"
 		return redirect('/')
+
+	if (session.get('error')!= None):
+		#blank out error message
+		session['error'] = ""
 
 	return render_template("result.html",name=name,location=location,language=language,comment=comment)
 
